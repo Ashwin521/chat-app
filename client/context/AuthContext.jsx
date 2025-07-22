4983fa1cec03968a433b8371835cd23d17751b08
Alkaison <505ganeshmourya@gmail.com>
2025-07-22 20:55:01 +0530

    fix: check endpoint route

--- client/context/AuthContext.jsx
+++ client/context/AuthContext.jsx
@@ -34,7 +34,7 @@ export const AuthProvider = ({ children }) => {
 
   const checkAuth = async () => {
     try {
-      const { data } = await axios.get("/api/user/check");
+      const { data } = await axios.get("/api/auth/check");
       if (data.success) {
         setAuthUser(data.user);
         connectSocket(data.user);
@@ -78,7 +78,7 @@ export const AuthProvider = ({ children }) => {
 
   const updateProfile = async (body) => {
     try {
-      const { data } = await axios.put("/api/user/update-profile", body);
+      const { data } = await axios.put("/api/auth/update-profile", body);
       if (data.success) {
         setAuthUser(data.user);
         toast.success("Profile updated successfully");
