51078afc2c3a3f651ad3f8e3f263ce3ef6838768
Ashwin <ashwingiri521@gmail.com>
2025-07-22 15:34:45 +0530

    added vercel.json

--- client/src/components/RightSideBar.jsx
+++ client/src/components/RightSideBar.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState, useEffect } from "react";
-import assets, { imagesDummyData } from "../assets/assets";
+import assets from "../assets/assets";
 import { AuthContext } from "../../context/AuthContext";
 import { ChatContext } from "../../context/ChatContext";
 
